15a513446e4580593345bc86d0606e4592da498b
Emma Högdal <emmaah98@outlook.com>
2026-04-03 22:35:12 +0200

    demoAccount tweaked for digitalResume

--- Client/src/components/chat/chatMenu.tsx
+++ Client/src/components/chat/chatMenu.tsx
@@ -1,12 +1,18 @@
 import { useEffect, useState } from "react";
 import "../../assets/styles/chatPage.css";
 import PersonAddIcon from "@mui/icons-material/PersonAdd";
-import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import type { ChatMenuProps } from "../../props/chatProp";
 import {
   LIVE_URL,
   // LOCAL_URL
 } from "../../url";
+
+const mockedUsers = [
+  { id: 1, fname: "Anna", lname: "Andersson", isOnline: true },
+  { id: 2, fname: "Kalle", lname: "Karlsson", isOnline: false },
+  { id: 3, fname: "Peter", lname: "Pettersson", isOnline: false },
+  { id: 4, fname: "Oskar", lname: "Oskarsson", isOnline: true },
+];
 
 export default function ChatMenu({ onSelectUser, onlineUsers }: ChatMenuProps) {
   const [users, setUsers] = useState([]);
@@ -21,7 +27,6 @@ export default function ChatMenu({ onSelectUser, onlineUsers }: ChatMenuProps) {
       });
 
       const res = await response.json();
-      console.log("USERS", res.users.data);
       const sortedUsers = res.users.data.sort((a: any, b: any) =>
         a.fname.localeCompare(b.fname),
       );
@@ -38,48 +43,96 @@ export default function ChatMenu({ onSelectUser, onlineUsers }: ChatMenuProps) {
         <h3 className="chatMenuHeader">Minichattare</h3>
       </div>
       <ul>
-        {users.map((user: any) => {
-          const isOnline = onlineUsers.includes(user.id);
-          return (
-            user.id !== currentUser.id && (
-              <div
-                className="item"
-                key={user.id}
-                onClick={() => {
-                  onSelectUser(user);
-                  console.log("Selected user", user.id);
-                }}
-              >
-                <div className="avatar">
-                  {user?.profilePicURL ? (
-                    <img
-                      src={user?.profilePicURL}
-                      style={{
-                        width: 32,
-                        height: 32,
-                        borderRadius: "50%",
-                        objectFit: "cover",
-                      }}
-                    />
-                  ) : (
-                    <div className="chatHeaderAvatar">
-                      <p>{user?.fname?.charAt(0).toUpperCase()}{user?.lname?.charAt(0).toUpperCase()}</p>
-                    </div>
-                  )}
-                  <span
-                    className={`status ${isOnline ? "online" : "offline"}`}
-                  ></span>
+        {currentUser.id === 34 &&
+          mockedUsers.map(
+            (user: any) =>
+              user.id !== currentUser.id && (
+                <div
+                  className="item"
+                  key={user.id}
+                  onClick={() => {
+                    onSelectUser(user);
+                    console.log("Selected user", user.id);
+                  }}
+                >
+                  <div className="avatar">
+                    {user?.profilePicURL ? (
+                      <img
+                        src={user?.profilePicURL}
+                        style={{
+                          width: 32,
+                          height: 32,
+                          borderRadius: "50%",
+                          objectFit: "cover",
+                        }}
+                      />
+                    ) : (
+                      <div className="chatHeaderAvatar">
+                        <p>
+                          {user?.fname?.charAt(0).toUpperCase()}
+                          {user?.lname?.charAt(0).toUpperCase()}
+                        </p>
+                      </div>
+                    )}
+                    <span
+                      className={`status ${user.isOnline ? "online" : "offline"}`}
+                    ></span>
+                  </div>
+                  <li className="miniChatters">
+                    {user.fname} {user.lname}
+                  </li>
+                  <div className="icon">
+                    <PersonAddIcon fontSize="small" />
+                  </div>
                 </div>
-                <li className="miniChatters">
-                  {user.fname} {user.lname}
-                </li>
-                <div className="icon">
-                  <PersonAddIcon fontSize="small" />
+              ),
+          )}
+        {currentUser.id !== 34 &&
+          users.map((user: any) => {
+            const isOnline = onlineUsers.includes(user.id);
+            return (
+              user.id !== currentUser.id && (
+                <div
+                  className="item"
+                  key={user.id}
+                  onClick={() => {
+                    onSelectUser(user);
+                    console.log("Selected user", user.id);
+                  }}
+                >
+                  <div className="avatar">
+                    {user?.profilePicURL ? (
+                      <img
+                        src={user?.profilePicURL}
+                        style={{
+                          width: 32,
+                          height: 32,
+                          borderRadius: "50%",
+                          objectFit: "cover",
+                        }}
+                      />
+                    ) : (
+                      <div className="chatHeaderAvatar">
+                        <p>
+                          {user?.fname?.charAt(0).toUpperCase()}
+                          {user?.lname?.charAt(0).toUpperCase()}
+                        </p>
+                      </div>
+                    )}
+                    <span
+                      className={`status ${isOnline ? "online" : "offline"}`}
+                    ></span>
+                  </div>
+                  <li className="miniChatters">
+                    {user.fname} {user.lname}
+                  </li>
+                  <div className="icon">
+                    <PersonAddIcon fontSize="small" />
+                  </div>
                 </div>
-              </div>
-            )
-          );
-        })}
+              )
+            );
+          })}
       </ul>
     </div>
   );
